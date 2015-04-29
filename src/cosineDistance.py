cc30aef8a8178ff3838e59f863bec0d7fe90a2f1
SZanlongo <SZanlongo@users.noreply.github.com>
2015-04-29 16:28:08 -0400

    Completed cosineDistance

--- src/cosineDistance.py
+++ src/cosineDistance.py
@@ -1,4 +1,10 @@
-import re
+# Calculate document distance given two files
+# Uses cosine formula described on Wikipedia: https://en.wikipedia.org/wiki/Cosine_similarity
+# Based on implementation by vpekar: http://stackoverflow.com/q/15173225
+
+from collections import Counter
+import re, math
+
 
 # regular expression
 # \w matches any alphanumeric character and the underscore
@@ -7,3 +13,32 @@ WORD = re.compile(r'\w+')
 
 def textToVector(text):
     words = WORD.findall(text)
+    # unordered collection where elements are stored as dict keys, and counts are stored as dict vals
+    return Counter(words)
+
+def cosDistance(vector1, vector2):
+    # set of unordered collection of unique items
+    intersection = set(vector1.keys()) & set(vector2.keys())  # return set with elements in intersection
+    numerator = sum([vector1[x] * vector2[x] for x in intersection])
+
+    sum1 = sum([vector1[x] ** 2 for x in vector1.keys()])
+    sum2 = sum([vector2[x] ** 2 for x in vector2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+def readFile(fileName):
+    return open("../data/" + fileName, 'r').read()
+
+text1 = readFile("nowIsTheTime.txt")
+text2 = readFile("quickBrownFox.txt")
+
+vector1 = textToVector(text1)
+vector2 = textToVector(text2)
+
+cosine = cosDistance(vector1, vector2)
+
+print "Cosine Distance:\t", cosine
